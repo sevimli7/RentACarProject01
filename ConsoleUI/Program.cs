f0b2d71fd3a3144512bab00296143eac369aaa0f
sevimli7 <jevelbrillant@gmail.com>
2021-04-10 19:40:20 +0300

    Düzenleme01

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleUI
             BrandManager brandManager = new BrandManager(new InMemoryCarDal());
             ColorManager colorManager = new ColorManager(new InMemoryCarDal());
             carManager.Add(new Car { CarId=6, BrandId=2, ColorId=3, ModelYear=2002,DailyPrice=600, Description="KIZILCAHAMAM NAKLİYAT"});
+           
             carManager.Update(new Car { CarId=1, ColorId=3, ModelYear=2002, BrandId=2, DailyPrice=900, Description="TWEEN Collections"});
             brandManager.Add(new Brand{BrandId=4, BrandName="HONDA" });
             brandManager.Delete(new Brand{ BrandId=4});
